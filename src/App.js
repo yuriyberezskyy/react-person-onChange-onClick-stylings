18ed109393ca7cb8c8a66b0ae3cb74cb25b24039
yuriyberezskyy <bereza95@gmail.com>
2020-04-30 21:19:15 -0400

    second condional way to hide

--- src/App.js
+++ src/App.js
@@ -51,33 +51,37 @@ class App extends Component {
       cursor: "pointer",
     };
 
+    let persons = null;
+    if (this.state.showPerson) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            changed={this.nameChangeHandler}
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+          >
+            My Hobbies: Racing
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         <button style={style} onClick={this.togglePersonHandle}>
-          Switch Name
+          Toggle persons
         </button>
-
-        {this.state.showPerson ? (
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            />
-            <Person
-              changed={this.nameChangeHandler}
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-            >
-              My Hobbies: Racing
-            </Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-            />
-          </div>
-        ) : null}
+        {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
